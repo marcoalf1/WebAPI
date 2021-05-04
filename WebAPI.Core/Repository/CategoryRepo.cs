610e19eb0564d2c2d43c0d575d564a982b421bc0
Marco Alfredo Hernández López <mahernandez@minex.gob.gt>
2021-05-04 13:17:17 -0600

    Category Repository created

---
+++ WebAPI.Core/Repository/CategoryRepo.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using WebAPI.Core.Models.NorthwindDB;
+
+namespace WebAPI.Core.Repository
+{
+    public class CategoryRepo : ICategoryRepo
+    {
+        public IEnumerable<Categories> GetCategories()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Categories GetCategory(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
+
---
+++ WebAPI.Core/Repository/ICategoryRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Core.Models.NorthwindDB;
+
+namespace WebAPI.Core.Repository
+{
+    public interface ICategoryRepo
+    {
+        IEnumerable<Categories> GetCategories();
+        Categories GetCategory(int id);
+        
+    }
+}
